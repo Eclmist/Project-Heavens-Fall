a0727061a117e0ebb2778db36b8609765af4eb46
Eclmist <Eclmist@yahoo.com>
2016-12-26 19:50:56 +0800

    Added test scene

---
+++ Assets/Editor/RTLightsEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RTLights))]
+public class RTLightsEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        RTLights light = (RTLights)target;
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        Handles.color = Color.white;
+        Handles.DrawWireArc(light.transform.position, Vector3.forward,
+            Vector3.right, light.angle, light.radius);
+
+
+    }
+}
---
+++ Assets/Rendering/RT Lights/RTLights.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(Light))]
+public class RTLights : MonoBehaviour
+{
+
+    [Range(0, 360)] public float angle;
+    [Range(0, 100)] public int resolution;
+    [Range(0, 100)] public int edgeResolveResolution;
+    [Range(0, 10)] public float distanceBias = 1;
+
+    public LayerMask shadowLayer;
+
+    /* Inherited Properties */
+    private Light light;
+    public float radius;
+    /* -------------------- */
+
+    private MeshFilter lightMeshFilter;
+    private Mesh lightMesh;
+
+    private List<Vector3> vertexList = new List<Vector3>();
+
+    void Start ()
+    {
+        light = GetComponent<Light>();
+    }
+	
+	void Update ()
+	{
+        radius = light.range;
+	}
+
+    public Vector3 DirFromAngle(float angle)
+    {
+        return new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0);
+    }
+}
