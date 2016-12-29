ecf386f1495b43a121610ae2b1192074688f3a20
Eclmist <Eclmist@yahoo.com>
2016-12-29 18:25:43 +0800

    Main menu update
    Added a main menu
    Added fonts
    Added more audio

---
+++ Assets/Animations/MainMenuUI/FontSizeAnimator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FontSizeAnimator : MonoBehaviour {
+
+    private enum FontSize
+    {
+        Selected = 18,
+        Default = 12
+    }
+
+    private FontSize fontSize = FontSize.Default;
+    private FontSize coroutineTarget = FontSize.Default;
+
+    private Text text;
+    private Button btn;
+
+    public void Start()
+    {
+        text = GetComponent<Text>();
+        btn = GetComponentInParent<Button>();
+    }
+
+    public void UpscaleText()
+    {
+        if (btn.interactable)
+            fontSize = FontSize.Selected;
+    }
+
+    public void DownscaleText()
+    {
+        if (btn.interactable)
+            fontSize = FontSize.Default;
+    }
+
+    public void Update()
+    {
+        if (coroutineTarget != fontSize)
+        {
+            StopAllCoroutines();
+            coroutineTarget = fontSize;
+            StartCoroutine(AnimateFontSize((int)fontSize));
+        }
+    }
+
+    IEnumerator AnimateFontSize(int size)
+    {
+        int currentSize = text.fontSize;
+        int difference = Mathf.Abs(size - currentSize);
+
+
+        for (int i = 0; i < difference; i++)
+        {
+            text.fontSize = (int)Mathf.Lerp(currentSize, size, ((float)i / (float)difference));
+            yield return new WaitForFixedUpdate();
+        }
+
+    }
+}
---
+++ Assets/Scripts/CameraFollowCursor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowCursor : MonoBehaviour
+{
+    [Range(0, 2)] public float cameraShakeMagnitude = 1;
+    [Range(0, 2)] public float cameraShakeFrequency = 1;
+    [Range(0, 2)] public float cameraShakeSpeed = 1;
+
+    Vector2 startingPosition;
+    Vector2 centerPoint;
+    Vector2 targetPoint;
+
+    float tParam = 2;
+    float scriptStartTime;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+        centerPoint = startingPosition;
+
+        gameObject.GetComponent<RenderImage>().setBrightness(0);
+        scriptStartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - scriptStartTime < 1)
+        {
+            gameObject.GetComponent<RenderImage>().setBrightness(Mathf.Lerp(0, 0.88F, Time.time - scriptStartTime));
+        }
+
+        transform.position = new Vector3(centerPoint.x + (Input.mousePosition.x - centerPoint.x) * 0.001F,
+                                          centerPoint.y + (Input.mousePosition.y - centerPoint.y) * 0.001F,
+                                          transform.position.z);
+
+        if (tParam < 1 / cameraShakeFrequency)
+        {
+            tParam += Time.deltaTime;
+            centerPoint = Vector2.Lerp(centerPoint, targetPoint, Time.deltaTime * cameraShakeSpeed);
+        }
+        else
+        {
+            targetPoint = startingPosition + Random.insideUnitCircle.normalized * cameraShakeMagnitude;
+            tParam = 0;
+        }
+    }
+}
