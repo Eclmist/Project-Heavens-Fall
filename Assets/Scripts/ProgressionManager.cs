cae79de6e56f840dce2d54e89b6685b99977406f
DarkDestry <tanki123@hotmail.com>
2016-12-31 01:03:46 +0800

    Added Progression Manager
    Finished Serialization for Player progression

---
+++ Assets/Scripts/ProgressionManager.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using UnityEngine;
+
+
+
+public class ProgressionManager : MonoBehaviour
+{
+    private static PlayerProgression currentProfile;
+
+    public static PlayerProgression CurrentProfile
+    {
+        get { return currentProfile; }
+    }
+
+    public static void CreateNewProfile(string profileName = "DefaultProfile")
+    {
+        currentProfile = new PlayerProgression(profileName);
+    }
+
+    public static string[] GetSavedProfiles()
+    {
+        string[] savefiles = System.IO.Directory.GetFiles(Application.persistentDataPath, "*.save");
+        for (int i = 0; i < savefiles.Length; i++)
+        {
+            savefiles[i] = savefiles[i].Substring(Application.persistentDataPath.Length + 1);
+            savefiles[i] = savefiles[i].Substring(0, savefiles[i].Length - 5);
+        }
+
+        return savefiles;
+    }
+
+    public static bool LoadProfile(string profileName = "DefaultProfile")
+    {
+        if (!File.Exists(Application.persistentDataPath + "/" + currentProfile.profileName + ".save")) return false;
+
+        FileStream file = new FileStream(
+            Application.persistentDataPath + "/" + profileName + ".save", FileMode.Open);
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PlayerProgression));
+            BinaryFormatter bf = new BinaryFormatter();
+
+            //TODO
+            currentProfile = (PlayerProgression) bf.Deserialize(file);
+            currentProfile.ReconstructDictionary();
+            //print("Load"+currentProfile.names.Count + currentProfile.LevelsUnlocked);
+            file.Close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            print(e);
+            return false;
+        }
+        finally
+        {
+            file.Close();
+        }
+    }
+
+    public static bool SaveProfile()
+    {
+        FileStream file = new FileStream(
+            Application.persistentDataPath + "/" + currentProfile.profileName + ".save", FileMode.OpenOrCreate);
+        try
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PlayerProgression));
+            BinaryFormatter bf = new BinaryFormatter();
+
+            //TODO
+            //print("Save" + currentProfile.names.Count + currentProfile.LevelsUnlocked);
+            bf.Serialize(file, currentProfile);
+            file.Close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            print(e);
+            return false;
+        }
+        finally
+        {
+            file.Close();
+        }
+    }
+
+    public static bool DeleteProfile(string profileName)
+    {
+        if (!File.Exists(Application.persistentDataPath + "/" + profileName + ".save")) return false;
+        File.Delete(Application.persistentDataPath + "/" + profileName + ".save");
+        return true;
+    }
+}
+
+[Serializable]
+public class PlayerProgression
+{
+    //public string LevelsUnlocked = "";
+    public string profileName;
+    //public List<string> names = new List<string>();
+    public List<StringLevelPair> StringLevelPairs = new List<StringLevelPair>();
+    [NonSerialized]private Dictionary<string,Level> PlayedLevels = new Dictionary<string, Level>();
+
+    [Serializable]
+    public struct StringLevelPair
+    {
+        public string str;
+        public Level lvl;
+
+        public StringLevelPair(string str, Level lvl)
+        {
+            this.str = str;
+            this.lvl = lvl;
+        }
+    }
+
+    [Serializable]
+    public class Level
+    {
+        public bool cleared;
+        public int checkpointIndex;
+        //public List<bool> levelLights;
+
+        public Level()
+        {
+            //TODO: Add accessors for this list
+            //levelLights = new List<bool>();
+            this.checkpointIndex = 0;
+            this.cleared = false;
+        }
+    }
+
+    public PlayerProgression()
+    {
+        //DONT CALL THIS
+    }
+
+    public PlayerProgression(string profileName)
+    {
+        this.profileName = profileName;
+    }
+
+    public bool IsLevelCleared(string levelId)
+    {
+        //return names.Contains(levelId);
+        if (PlayedLevels.ContainsKey(levelId)) return PlayedLevels[levelId].cleared;
+        return false;
+    }
+
+    public int GetCheckpointIndex(string levelId)
+    {
+        Assert.SoftAssert(PlayedLevels.ContainsKey(levelId), "You shouldnt be trying to get the checkpoint of a level you havent played." + StringLevelPairs.Count);
+        if (PlayedLevels.ContainsKey(levelId)) return PlayedLevels[levelId].checkpointIndex;
+        return -1;
+    }
+    
+    public void SetLevelCheckpoint(string levelId, int checkpointIndex)
+    {
+        if (!PlayedLevels.ContainsKey(levelId)) AddNewLevelEntry(levelId, new Level());
+        PlayedLevels[levelId].checkpointIndex = checkpointIndex;
+    }
+
+    public void UnlockLevel(string levelId)
+    {
+        if (!PlayedLevels.ContainsKey(levelId)) AddNewLevelEntry(levelId, new Level());
+    }
+
+    public bool IsLevelUnlocked(string levelId)
+    {
+        return PlayedLevels.ContainsKey(levelId);
+    }
+
+    public void SetLevelCleared(string levelId)
+    {
+        //AddNewLevelEntry(levelId,new Level());
+
+        Assert.SoftAssert(PlayedLevels.ContainsKey(levelId), "You shouldnt be clearing a level that you havent played yet. No Action Taken");
+        if (PlayedLevels.ContainsKey(levelId)) PlayedLevels[levelId].cleared = true;
+    }
+
+    public void OverrideLevelCleared(string levelId)
+    {
+        //AddNewLevelEntry(levelId, new Level());
+
+
+        Debug.LogWarning("<color=#FF0000>Overriding Level cleared status. Creating entry if it doesnt exist</color>" + StringLevelPairs.Count);
+        if (!PlayedLevels.ContainsKey(levelId)) AddNewLevelEntry(levelId, new Level());
+        PlayedLevels[levelId].cleared = true;
+    }
+
+    private void AddNewLevelEntry(string str, Level lvl)
+    {
+        StringLevelPairs.Add(new StringLevelPair(str, lvl));
+        //LevelsUnlocked += "$" + str;
+        //names.Add(str);
+        PlayedLevels.Add(str, lvl);
+    }
+
+    public void ReconstructDictionary()
+    {
+        //char[] delimiters = {'$'};
+        //string[] levels = LevelsUnlocked.Split(delimiters,StringSplitOptions.RemoveEmptyEntries);
+        //names.AddRange(levels);
+
+        PlayedLevels = new Dictionary<string, Level>();
+        foreach (var stringLevelPair in StringLevelPairs)
+        {
+            PlayedLevels.Add(stringLevelPair.str, stringLevelPair.lvl);
+        }
+    }
+}
+
---
+++ Assets/Scripts/UnitTests/Editor/ProgressionManagerTester.cs
@@ -0,0 +1,188 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using UnitAssert = NUnit.Framework.Assert;
+
+public class ProgressionManagerTester {
+
+	[Test]
+	public void ProfileCreationAndDeletion()
+	{
+
+        ProgressionManager.DeleteProfile("TestProfile1");
+        ProgressionManager.DeleteProfile("TestProfile2");
+        ProgressionManager.DeleteProfile("TestProfile3");
+
+        string[] profiles;
+
+        //Creation
+        ProgressionManager.CreateNewProfile("TestProfile1");
+
+        UnitAssert.AreNotEqual(ProgressionManager.CurrentProfile, null);
+
+        UnitAssert.IsTrue(ProgressionManager.SaveProfile(), "Failed to Save profile");
+	    profiles = ProgressionManager.GetSavedProfiles();
+
+        //Assert
+        UnitAssert.AreNotEqual(profiles.Length, 0);
+        UnitAssert.AreEqual("TestProfile1", profiles[0]);
+
+        //Deletion
+	    ProgressionManager.DeleteProfile("TestProfile1");
+	    profiles = ProgressionManager.GetSavedProfiles();
+
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile1"), "Profile still Exist after deletion");
+	}
+
+    [Test]
+    public void ProfileListing()
+    {
+        
+        ProgressionManager.DeleteProfile("TestProfile1");
+        ProgressionManager.DeleteProfile("TestProfile2");
+        ProgressionManager.DeleteProfile("TestProfile3");
+
+        string[] profiles;
+
+        //Create multiple Profiles
+        ProgressionManager.CreateNewProfile("TestProfile1");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile2");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile3");
+        ProgressionManager.SaveProfile();
+
+        profiles = ProgressionManager.GetSavedProfiles();
+        UnitAssert.AreEqual(3, profiles.Length);
+        UnitAssert.Contains("TestProfile1", profiles);
+        UnitAssert.Contains("TestProfile2", profiles);
+        UnitAssert.Contains("TestProfile3", profiles);
+
+        //Deletion
+        ProgressionManager.DeleteProfile("TestProfile1");
+        ProgressionManager.DeleteProfile("TestProfile2");
+        ProgressionManager.DeleteProfile("TestProfile3");
+        profiles = ProgressionManager.GetSavedProfiles();
+
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile1"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile2"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile3"), "Profile still Exist after deletion");
+    }
+
+    [Test]
+    public void ProfilePersistance()
+    {
+        string[] profiles;
+
+
+        //Create multiple Profiles
+        ProgressionManager.CreateNewProfile("TestProfile1");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel1");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel2");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel3");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile2");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel4");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel5");
+        ProgressionManager.CurrentProfile.OverrideLevelCleared("UnitTestLevel6");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile3");
+        ProgressionManager.SaveProfile();
+
+        ProgressionManager.LoadProfile("TestProfile1");
+        UnitAssert.AreEqual("TestProfile1", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel1"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel2"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel3"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel4"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel5"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel6"));
+        ProgressionManager.LoadProfile("TestProfile2");
+        UnitAssert.AreEqual("TestProfile2", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel1"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel2"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel3"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel4"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel5"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel6"));
+        ProgressionManager.LoadProfile("TestProfile3");
+        UnitAssert.AreEqual("TestProfile3", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel1"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel2"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel3"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel4"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel5"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelCleared("UnitTestLevel6"));
+
+        //Deletion
+        ProgressionManager.DeleteProfile("TestProfile1");
+        ProgressionManager.DeleteProfile("TestProfile2");
+        ProgressionManager.DeleteProfile("TestProfile3");
+        profiles = ProgressionManager.GetSavedProfiles();
+
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile1"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile2"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile3"), "Profile still Exist after deletion");
+    }
+
+    [Test]
+    public void LevelUnlockTester()
+    {
+
+        string[] profiles;
+
+
+        //Create multiple Profiles
+        ProgressionManager.CreateNewProfile("TestProfile1");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel1");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel2");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel3");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile2");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel4");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel5");
+        ProgressionManager.CurrentProfile.UnlockLevel("UnitTestLevel6");
+        ProgressionManager.SaveProfile();
+        ProgressionManager.CreateNewProfile("TestProfile3");
+        ProgressionManager.SaveProfile();
+
+
+        ProgressionManager.LoadProfile("TestProfile1");
+        UnitAssert.AreEqual("TestProfile1", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel1"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel2"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel3"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel4"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel5"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel6"));
+        ProgressionManager.LoadProfile("TestProfile2");
+        UnitAssert.AreEqual("TestProfile2", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel1"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel2"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel3"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel4"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel5"));
+        UnitAssert.IsTrue(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel6"));
+        ProgressionManager.LoadProfile("TestProfile3");
+        UnitAssert.AreEqual("TestProfile3", ProgressionManager.CurrentProfile.profileName);
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel1"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel2"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel3"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel4"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel5"));
+        UnitAssert.IsFalse(ProgressionManager.CurrentProfile.IsLevelUnlocked("UnitTestLevel6"));
+
+
+        //Deletion
+        ProgressionManager.DeleteProfile("TestProfile1");
+        ProgressionManager.DeleteProfile("TestProfile2");
+        ProgressionManager.DeleteProfile("TestProfile3");
+        profiles = ProgressionManager.GetSavedProfiles();
+
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile1"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile2"), "Profile still Exist after deletion");
+        UnitAssert.IsTrue(!profiles.Contains("TestProfile3"), "Profile still Exist after deletion");
+    }
+}
