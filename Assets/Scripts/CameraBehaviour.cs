8a8189456e026ada5c14d2e9f2dde767302a9b73
Eclmist <Eclmist@yahoo.com>
2016-12-31 18:07:32 +0800

    Modified camera Removed redundant RT lights in scene

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -7,14 +7,14 @@ public class CameraBehaviour : MonoBehaviour
 
     public static CameraBehaviour Instance;
 
-    public float CameraSpeed = 6;
+    public float cameraSpeed = 6;
     public float yUpperBounds = 18; 
-    public float yLowerBounds = -5;
+    public float yLowerBounds = -3;
 
     public GameObject target;
 
     float cameraZoffset;
-
+    private Vector3 centerPos;
     void Awake()
     {
         Instance = this;
@@ -22,12 +22,15 @@ public class CameraBehaviour : MonoBehaviour
 
     void Start()
     {
+        centerPos = transform.position;
         cameraZoffset = transform.position.z;
     }
 
 
     // Update is called once per frame
-    void LateUpdate () {
+    void LateUpdate ()
+    {
+
 
         if (target == null)
         {
@@ -35,10 +38,16 @@ public class CameraBehaviour : MonoBehaviour
         }
         else
         {
-            Vector3 targetPos = new Vector3(target.transform.position.x, Mathf.Clamp(target.transform.position.y, yLowerBounds, yUpperBounds), cameraZoffset);
-            transform.position = Vector3.Lerp(transform.position, targetPos, CameraSpeed * Time.deltaTime);
+            centerPos = new Vector3(target.transform.position.x, Mathf.Clamp(target.transform.position.y, yLowerBounds, yUpperBounds), cameraZoffset);
         }
-	}
+
+        Vector3 targetPos = new Vector3(centerPos.x + (Input.mousePosition.x - centerPos.x) * 0.001F,
+                                          centerPos.y + (Input.mousePosition.y - centerPos.y) * 0.001F,
+                                          transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime*cameraSpeed);
+
+    }
 
     void setCameraMaxMin(float max, float min)
     {
