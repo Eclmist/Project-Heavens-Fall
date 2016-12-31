6d2c918f5e4307a501e0a9fb051b4d767782f1aa
Eclmist <Eclmist@yahoo.com>
2016-12-31 17:45:32 +0800

    Added player spawn and death

--- Assets/Scripts/Player/PlayerControllerA.cs
+++ Assets/Scripts/Player/PlayerControllerA.cs
@@ -1,7 +1,10 @@
 ﻿ using UnityEngine;
 using System.Collections;
 
-public class PlayerControllerA : MonoBehaviour {
+public class PlayerControllerA : MonoBehaviour
+{
+
+    public static PlayerControllerA Instance;
 
     //Player
     public float maxJump = 8;
@@ -15,7 +18,7 @@ public class PlayerControllerA : MonoBehaviour {
 
     float fAccel;
     float jumpVelocity;
-    static Vector3 vDirection;
+    Vector3 vDirection;
     float sVelocity;
 
     float rayOffset = 0.1f;
@@ -109,7 +112,7 @@ public class PlayerControllerA : MonoBehaviour {
         Move(vDirection * Time.deltaTime);
     }
 
-    static void addForce(Vector3 f)
+    public void addForce(Vector3 f)
     {
         vDirection += f;
     }
