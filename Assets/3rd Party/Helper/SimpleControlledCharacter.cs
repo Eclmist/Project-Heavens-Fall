42730534efa848635a47c1d6de7a72be69666e35
DarkDestry <tanki123@hotmail.com>
2017-01-01 17:01:47 +0800

    Added Prototype level
    Added Ghost mode

--- Assets/3rd Party/Helper/SimpleControlledCharacter.cs
+++ Assets/3rd Party/Helper/SimpleControlledCharacter.cs
@@ -16,21 +16,38 @@ namespace UsefulStuff
         public KeyCode up = KeyCode.W;
         public KeyCode down = KeyCode.S;
 
+        private PlayerControllerA controller;
+        private DeathOnImpact death;
+
+        private bool ghosting;
 
         // Use this for initialization
         void Start()
         {
-
+            controller = GetComponent<PlayerControllerA>();
+            death = GetComponent<DeathOnImpact>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                ghosting = !ghosting;
+                controller.enabled = !ghosting;
+                death.enabled = !ghosting;
+            }
+
+            if (!ghosting) return;
+
+            float tempSpeed = speed;
+            if (Input.GetKey(KeyCode.LeftShift)) tempSpeed *= 3;
+
 
             if (!useCustomKeycodes)
                 transform.position += (Input.GetAxis("Horizontal")*Vector3.right +
-                                       Input.GetAxis("Vertical")*Vector3.forward)*
-                                      Time.deltaTime*speed;
+                                       Input.GetAxis("Vertical")*Vector3.up)*
+                                      Time.deltaTime* tempSpeed;
             else
             {
                 Vector3 axis = Vector3.zero;
@@ -38,7 +55,7 @@ namespace UsefulStuff
                 if (Input.GetKey(right)) axis += Vector3.right;
                 if (Input.GetKey(up)) axis += Vector3.up;
                 if (Input.GetKey(down)) axis += Vector3.down;
-                transform.position += axis*Time.deltaTime*speed;
+                transform.position += axis*Time.deltaTime* tempSpeed;
             }
         }
     }
