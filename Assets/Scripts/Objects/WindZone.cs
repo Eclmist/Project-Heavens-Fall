b788c59251f4c6845cbcbca9c3f41242da08887b
iAbee <abraham_1360@hotmail.com>
2017-01-02 17:06:34 +0800

    WindZone

---
+++ Assets/Scripts/Objects/WindZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZone : MonoBehaviour {
+
+    public ParticleSystem windParticle;
+
+    public Vector3 windForce = new Vector3(0.6f, 0, 0);
+    public float windSize = 2.0f;
+    public Vector3 windDir = new Vector3(1, 0, 0); 
+
+	// Use this for initialization
+	void Start () {
+
+
+        //ParticleSystem.ShapeModule windShape = windParticle.shape;
+        //windShape.radius = windSize;
+        //ParticleSystem.EmissionModule windEmission = windParticle.emission;
+        //windEmission.rateOverTime = windSize * 50.0f;
+        
+        transform.LookAt(windDir);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerControllerA>().addForce(windForce);
+        }
+    }
+}
