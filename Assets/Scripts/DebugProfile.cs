a7d997f137d0ebb16d6e2b15f723ebc21a89fe33
DarkDestry <tanki123@hotmail.com>
2017-01-07 01:18:24 +0800

    Added Disabled Object Guard for Player, Main Camera and LevelEndpoint

--- Assets/Scripts/DebugProfile.cs
+++ Assets/Scripts/DebugProfile.cs
@@ -12,6 +12,20 @@ public class DebugProfile : MonoBehaviour {
 	        ProgressionManager.CreateNewProfile();
             ProgressionManager.CurrentProfile.SetLevelUnlocked(SceneManager.GetActiveScene().name);
 	    }
+
+	    GameObject[] objs = SceneManager.GetActiveScene().GetRootGameObjects();
+
+
+        foreach (var obj in objs)
+	    {
+            if (obj.name == "Player" || obj.name == "Main Camera" || obj.name == "LevelEndPoint" || obj.name == "Checkpoint")
+	            obj.SetActive(true);
+        }
+
         Destroy(gameObject);
-	}
+
+        //Find("Player").SetActive(true);
+        //Find("LevelEndPoint").SetActive(true);
+        //Find("Main Camera").SetActive(true);
+    }
 }
