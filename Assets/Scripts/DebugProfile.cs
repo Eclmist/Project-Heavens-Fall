325adc1d89b475127aeca2470e15e7c542f2e940
DarkDestry <tanki123@hotmail.com>
2017-01-07 00:46:20 +0800

    Object Bank - Screw you Abraham and your no material prefabs
    - Imported and Reworked Object Bank.
    - Added Reference Scene V2 for Level Design Templating
    - Added Self Destruct script for any level design reference objects
    - Added Layer to ignore picking
    
    Level Changes:
    - Finished Level 01 (Untested) Waiting for main menu hotfix

--- Assets/Scripts/DebugProfile.cs
+++ Assets/Scripts/DebugProfile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DebugProfile : MonoBehaviour {
 
@@ -9,6 +10,7 @@ public class DebugProfile : MonoBehaviour {
 	    if (ProgressionManager.CurrentProfile == null)
 	    {
 	        ProgressionManager.CreateNewProfile();
+            ProgressionManager.CurrentProfile.SetLevelUnlocked(SceneManager.GetActiveScene().name);
 	    }
         Destroy(gameObject);
 	}
---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+class SelfDestruct : MonoBehaviour
+{
+    public void Start()
+    {
+        Destroy(gameObject);
+    }
+}
+
