8a862d325318ea52639020e9e62a40bd3e5db9d0
DarkDestry <tanki123@hotmail.com>
2017-02-26 14:57:23 +0800

    Added new unit tests for gap and player snapping

--- Assets/Scripts/UnitTests/CharacterControllerUnitTest.cs
+++ Assets/Scripts/UnitTests/CharacterControllerUnitTest.cs
@@ -38,6 +38,7 @@ public class CharacterControllerUnitTest : MonoBehaviour
 			if (!executeUnitTest) return;
 			executeUnitTest = false;
 			unitTestRunning = true;
+			//unitTestPhase = UnitTestPhases.flySnappingControlTest;
 
 			Text[] texts = FindObjectsOfType<Text>();
 			foreach (var text in texts)
@@ -344,7 +345,224 @@ public class CharacterControllerUnitTest : MonoBehaviour
 					}
 				}
 				break;
-		    default:
+			case UnitTestPhases.flySnappingControlTest:
+
+				//Arrange
+				if (state == TestState.start)
+				{
+					//Teleport player to location
+					testComponentObject = GameObject.Find("FlySnappingTestControlStartPoint");
+
+					timeout = 0;
+					useTimeout = false;
+					testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = false;
+					failureCondition = "Player does not snap to surface";
+					Player.Instance.transform.position = testComponentObject.transform.position;
+					state = TestState.running;
+					//PrintToCanvas(Color.yellow, unitTestPhase.ToString() + "\nRunning...");
+
+					MetricsTester.Instance.maxHeight = 0;
+
+				}
+
+				//Test
+				if (state == TestState.running)
+				{
+					if (timeElapsed > 1f)
+						testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = true;
+
+					if (Player.Instance.GetComponent<DeathOnImpact>().enabled == false)
+					{
+						state = TestState.cleanup;
+						TestFail("Player snapped to surface and died");
+					}
+
+					//Wait fort 5 seconds
+					if (timeElapsed > 3f)
+					{
+						state = TestState.cleanup;
+						//Assert
+						TestSuccess();
+
+
+					}
+
+				}
+
+
+				//Cleanup
+				if (state == TestState.cleanup)
+				{
+					//Teleport to Null zone and wait 2 seconds
+					if (!failureCondition.Equals(""))
+					{
+						DefaultCleanup();
+						Player.Instance.transform.position = GameObject.Find("NullZone").transform.position;
+					}
+
+					if (timeElapsed > 2)
+					{
+						state = TestState.start;
+						if (fpsLimit == FPS.low)
+						{
+							unitTestPhase++;
+							FrameLimiter.instance.sleepMillis = 10;
+							fpsLimit = FPS.high;
+						}
+						else
+						{
+							fpsLimit = FPS.low;
+							FrameLimiter.instance.sleepMillis = 30;
+						}
+						DefaultCleanup();
+					}
+				}
+				break;
+			case UnitTestPhases.flySnappingTest:
+
+				//Arrange
+				if (state == TestState.start)
+				{
+					//Teleport player to location
+					testComponentObject = GameObject.Find("FlySnappingTestStartPoint");
+
+					timeout = 0;
+					useTimeout = false;
+					testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = false;
+					failureCondition = "Player does not snap to surface";
+					Player.Instance.transform.position = testComponentObject.transform.position;
+					state = TestState.running;
+					//PrintToCanvas(Color.yellow, unitTestPhase.ToString() + "\nRunning...");
+
+					MetricsTester.Instance.maxHeight = 0;
+
+				}
+
+				//Test
+				if (state == TestState.running)
+				{
+					if (timeElapsed > 1f)
+						testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = true;
+
+					if (Player.Instance.GetComponent<DeathOnImpact>().enabled == false)
+					{
+						state = TestState.cleanup;
+						TestFail("Player snapped to surface and died");
+					}
+
+					//Wait fort 5 seconds
+					if (timeElapsed > 3f)
+					{
+						state = TestState.cleanup;
+						//Assert
+						TestSuccess();
+
+
+					}
+
+				}
+
+
+				//Cleanup
+				if (state == TestState.cleanup)
+				{
+					//Teleport to Null zone and wait 2 seconds
+					if (!failureCondition.Equals(""))
+					{
+						DefaultCleanup();
+						Player.Instance.transform.position = GameObject.Find("NullZone").transform.position;
+					}
+
+					if (timeElapsed > 2)
+					{
+						state = TestState.start;
+						if (fpsLimit == FPS.low)
+						{
+							unitTestPhase++;
+							FrameLimiter.instance.sleepMillis = 10;
+							fpsLimit = FPS.high;
+						}
+						else
+						{
+							fpsLimit = FPS.low;
+							FrameLimiter.instance.sleepMillis = 30;
+						}
+						DefaultCleanup();
+					}
+				}
+				break;
+			case UnitTestPhases.gapTest:
+
+				//Arrange
+				if (state == TestState.start)
+				{
+					//Teleport player to location
+					testComponentObject = GameObject.Find("gapTestStartPoint");
+
+					timeout = 0;
+					useTimeout = false;
+					testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = false;
+					failureCondition = "Player stuck in gap";
+					Player.Instance.transform.position = testComponentObject.transform.position;
+					state = TestState.running;
+					//PrintToCanvas(Color.yellow, unitTestPhase.ToString() + "\nRunning...");
+
+					MetricsTester.Instance.maxHeight = 0;
+
+				}
+
+				//Test
+				if (state == TestState.running)
+				{
+					if (timeElapsed > 1f)
+						testComponentObject.GetComponentInChildren<WindZone>().GetComponent<Collider>().enabled = true;
+
+
+					//Wait fort 5 seconds
+					if (timeElapsed > 5f)
+					{
+						state = TestState.cleanup;
+						//Assert
+						if (Player.Instance.transform.position.y < 2f)
+							TestSuccess();
+						
+						else
+							TestFail("Player stuck in gap");
+
+					}
+
+				}
+
+
+				//Cleanup
+				if (state == TestState.cleanup)
+				{
+					//Teleport to Null zone and wait 2 seconds
+					if (!failureCondition.Equals(""))
+					{
+						DefaultCleanup();
+						Player.Instance.transform.position = GameObject.Find("NullZone").transform.position;
+					}
+
+					if (timeElapsed > 2)
+					{
+						state = TestState.start;
+						if (fpsLimit == FPS.low)
+						{
+							unitTestPhase++;
+							FrameLimiter.instance.sleepMillis = 10;
+							fpsLimit = FPS.high;
+						}
+						else
+						{
+							fpsLimit = FPS.low;
+							FrameLimiter.instance.sleepMillis = 30;
+						}
+						DefaultCleanup();
+					}
+				}
+				break;
+			default:
 			    throw new ArgumentOutOfRangeException();
 	    }
 
@@ -393,6 +611,9 @@ public class CharacterControllerUnitTest : MonoBehaviour
 		highSpeedPlatformTest,
 		windzoneTest,
 		jumpTest,
+		flySnappingControlTest,
+		flySnappingTest,
+		gapTest,
 		end,
     }
 
